289cd399ea45dcf7300f391a76d57f663a992574
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-08-30 10:13:52 -0500

    game fetches and displays high scores

--- server/gameRoute.js
+++ server/gameRoute.js
@@ -4,21 +4,24 @@ var User = require('./userModel.js');
 module.exports = {
 
   getScores: function(req, res) {
-    // User.find({},{username:1, score:1})
-    User.find({},{username:true, score:true},{"sort":["score","desc"], "limit":10})
-      .toArray(function(err, data){
-        console.log("DB DATA: ",data);
-        if (err) {
-          console.log("mongo find getScores err: ", err);
-        } else {
-          res.json(data);
-        }
-      });
 
-      // .sort({score: -1})
-      // .limit(10)
-      // .then()
-      // .fail()
+    // User.find({},{username:true, score:true},{"sort":"score", "limit":10})
+    //   .toArray(function(err, data){
+    //     console.log("DB DATA: ",data);
+    //     if (err) {
+    //       console.log("mongo find getScores err: ", err);
+    //     } else {
+    //       res.json(data);
+    //     }
+    //   });
+
+      User.find({},{username:1, score:1})
+      .sort({score: -1})
+      .limit(10)
+      .then(function(data){
+        res.json(data);
+      })
+      // .fail(function(err){console.log("ERR: ", err)})
   },
 
   updateScore: function(req, res) {}
